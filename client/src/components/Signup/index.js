31c0d370c72291e2089fa4f5ae3596e9e8cd64b8
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-11-01 02:31:51 -0700

    init functionality for signing up/creating user, working minus edge cases

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -31,7 +31,7 @@ class Login extends React.Component {
                 users.map((user) => {
                     console.log(user);
 
-                    if (user.email === this.state.email) {
+                    if(user.email === this.state.email) {
                         alert("Email already in use...");
                         return;
                     }
@@ -41,11 +41,19 @@ class Login extends React.Component {
             }
         );
 
-        if (this.state.password != this.state.passwordCheck) {
+        if(this.state.password != this.state.passwordCheck) {
             alert("Passwords do not match...");
             return;
         }
 
+        API.createUser((this.state.firstName + " " + this.state.lastName), this.state.email, this.state.password)
+        .catch(err => {
+            console.log(err)
+        })
+        .then(() => {
+            this.setState({ firstName: "", lastName: "", email: "", password: "", passwordCheck: ""});
+            window.location.href = "/"
+        });
     }
 
     render() {
@@ -59,18 +67,18 @@ class Login extends React.Component {
                                 <form id="signup" className="col s12 ">
                                     <div className="row">
                                         <div className="input-field col m5 offset-m1 s10 offset-s1">
-                                            <input id="first-signin" type="text" className="validate" name="firstIn" value={this.state.firstName} onChange={this.handleChange} />
+                                            <input id="first-signin" type="text" className="validate" name="firstName" value={this.state.firstName} onChange={this.handleChange} />
                                             <label htmlFor="namein">First Name</label>
                                         </div>
                                         <div className="input-field col m5 s10 offset-s1">
-                                            <input id="last-signin" type="text" className="validate" name="lastIn" value={this.state.lastName} onChange={this.handleChange} />
+                                            <input id="last-signin" type="text" className="validate" name="lastName" value={this.state.lastName} onChange={this.handleChange} />
                                             <label htmlFor="namein">Last Name</label>
                                         </div>
                                     </div>
                                     <div className="row">
                                         <div className="input-field col s10 offset-s1">
                                             <input id="name-signin" type="text" className="validate" name="email" value={this.state.email} onChange={this.handleChange} />
-                                            <label htmlFor="namein">Enter Email to Sign Up</label>
+                                            <label htmlFor="namein">Enter Email to sign up...</label>
                                         </div>
                                     </div>
                                     <div className="row">
@@ -79,7 +87,7 @@ class Login extends React.Component {
                                             <label htmlFor="password">New Password</label>
                                         </div>
                                         <div className="input-field col m5 s10 offset-s1">
-                                            <input id="password2" type="password" className="validate" name="password2" value={this.state.passwordCheck} onChange={this.handleChange} />
+                                            <input id="password2" type="password" className="validate" name="passwordCheck" value={this.state.passwordCheck} onChange={this.handleChange} />
                                             <label htmlFor="password2">Verify Password</label>
                                         </div>
                                     </div>

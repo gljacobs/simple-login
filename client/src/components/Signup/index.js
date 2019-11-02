21e1d6d0258819d3597b34b2ec84eaa9307ed214
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-11-01 18:53:06 -0700

    add form validation and required fields for signup

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -24,36 +24,40 @@ class Signup extends React.Component {
         this.setState({ [name]: value })
     }
 
-    handleLogin = (event) => {
-        event.preventDefault();
-        API.getUsers()
-            .then(users =>
-                users.map((user) => {
-                    console.log(user);
+    handleLogin = () => {
+        if (this.state.firstName && this.state.lastName && this.state.email && this.state.password) {
+            let valid = true;
 
-                    if(user.email === this.state.email) {
-                        alert("Email already in use...");
+            API.getUsers()
+                .then(users =>
+                    users.map((user) => {
+                        if (user.email === this.state.email) {
+                            valid = false;
+                            alert("Email already in use...");
+                        }
+                    })
+                ).catch(err => {
+                    console.log(err)
+                }
+                )
+                .then(() => {
+                    if (this.state.password !== this.state.passwordCheck) {
+                        alert("Passwords do not match...");
                         return;
                     }
+                    else if (valid) {
+
+                        API.createUser((this.state.firstName + " " + this.state.lastName), this.state.email, this.state.password)
+                            .catch(err => {
+                                console.log(err)
+                            })
+                            .then(() => {
+                                this.setState({ firstName: "", lastName: "", email: "", password: "", passwordCheck: "" });
+                                window.location.href = "/"
+                            });
+                    }
                 })
-            ).catch(err => {
-                console.log(err)
-            }
-        );
-
-        if(this.state.password !== this.state.passwordCheck) {
-            alert("Passwords do not match...");
-            return;
         }
-
-        API.createUser((this.state.firstName + " " + this.state.lastName), this.state.email, this.state.password)
-        .catch(err => {
-            console.log(err)
-        })
-        .then(() => {
-            this.setState({ firstName: "", lastName: "", email: "", password: "", passwordCheck: ""});
-            window.location.href = "/"
-        });
     }
 
     render() {
@@ -67,28 +71,29 @@ class Signup extends React.Component {
                                 <form id="signup" className="col s12 ">
                                     <div className="row">
                                         <div className="input-field col m5 offset-m1 s10 offset-s1">
-                                            <input id="first-signin" type="text" className="validate" name="firstName" value={this.state.firstName} onChange={this.handleChange} />
+                                            <input id="first-signin" type="text" className="validate" name="firstName" value={this.state.firstName} onChange={this.handleChange} required />
                                             <label htmlFor="namein">First Name</label>
                                         </div>
                                         <div className="input-field col m5 s10 offset-s1">
-                                            <input id="last-signin" type="text" className="validate" name="lastName" value={this.state.lastName} onChange={this.handleChange} />
+                                            <input id="last-signin" type="text" className="validate" name="lastName" value={this.state.lastName} onChange={this.handleChange} required />
                                             <label htmlFor="namein">Last Name</label>
                                         </div>
                                     </div>
                                     <div className="row">
                                         <div className="input-field col s10 offset-s1">
-                                            <input id="name-signin" type="text" className="validate" name="email" value={this.state.email} onChange={this.handleChange} />
-                                            <label htmlFor="namein">Enter Email to sign up...</label>
+                                            <input id="email" type="email" className="validate" name="email" value={this.state.email} onChange={this.handleChange} required />
+                                            <label htmlFor="email">Enter Email to sign up...</label>
+                                            <span className="helper-text" data-error="Please enter a valid email" />
                                         </div>
                                     </div>
                                     <div className="row">
                                         <div className="input-field col m5 offset-m1 col s10 offset-s1">
-                                            <input id="password1" type="password" className="validate" name="password" value={this.state.password} onChange={this.handleChange} />
-                                            <label htmlFor="password">New Password</label>
+                                            <input id="password1" type="password" className="validate" name="password" value={this.state.password} onChange={this.handleChange} required />
+                                            <label htmlFor="password">{((this.state.password === this.state.passwordCheck) && this.state.password) ? "Passwords Match" : "New Password"}</label>
                                         </div>
                                         <div className="input-field col m5 s10 offset-s1">
-                                            <input id="password2" type="password" className="validate" name="passwordCheck" value={this.state.passwordCheck} onChange={this.handleChange} />
-                                            <label htmlFor="password2">Verify Password</label>
+                                            <input id="password2" type="password" className="validate" name="passwordCheck" value={this.state.passwordCheck} onChange={this.handleChange} required />
+                                            <label htmlFor="password2">{((this.state.password === this.state.passwordCheck) && this.state.password) ? "Passwords Match" : "Verify Password"}</label>
                                         </div>
                                     </div>
                                     <div className="row">
@@ -96,10 +101,10 @@ class Signup extends React.Component {
                                         <i className="material-icons right">send</i>
                                         </button>
                                     </div>
+                                    <div className="card-action">
+                                        <a href="/">Return to Login</a>
+                                    </div>
                                 </form>
-                            </div>
-                            <div className="card-action">
-                                <a href="/">Return to Login</a>
                             </div>
                         </div>
                     </div>

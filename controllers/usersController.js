a676b2b96962a1dc8216bab1bd5c07006e06eda4
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-11-05 14:36:46 -0800

    hash users password on signup

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,6 @@
 const db = require("../database/models");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 module.exports = {
     find: function (req, res) {
@@ -18,18 +20,21 @@ module.exports = {
         });
     },
     create: function (req, res) {
-        console.log(`Creating user ${req.body.email}`)
-        db.Users.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-        })
-            .then(function (response) {
-                res.sendStatus(200)
-            }).catch(function (err) {
-                console.error(err.original.sqlMessage)
-                res.sendStatus(400)
-            });
+        console.log(`Creating user ${req.body.email}`);
+        bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+            // Store hash in your password DB.
+            db.Users.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: hash,
+            })
+                .then(function (response) {
+                    res.sendStatus(200)
+                }).catch(function (err) {
+                    console.error(err.original.sqlMessage)
+                    res.sendStatus(400)
+                });
+        });
     },
     remove: function (req, res) {
         console.log(`Removing user ${req.params.email}`)
